913b9405352c91d613a3a03c2c66e8a4df118e27
Carlos Torres <carlos@yugioh.com>
2023-04-02 10:45:55 -0600

    implemented the login endpoint and added enviroment variables

--- index.js
+++ index.js
@@ -1,56 +1,99 @@
-const express = require('express');
 const cors = require('cors');
-const app = express();
+const express = require('express');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const mongoose = require('mongoose');
-const port = 3000;
+const session = require('express-session');
+require('dotenv').config()
+
+
+const app = express();
 app.use(cors());
+
+
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.REACT_APP_DB_HOST,
+  user:  process.env.REACT_APP_DB_USER,
+  password:  process.env.REACT_APP_DB_PASS,
+  database:  process.env.REACT_APP_DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+    return;
+  }
+
+
+
+  console.log('Connected to MySQL database!');
+});
+
+
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// Middleware
+app.use(express.json());
 
 
 app.get('/githubSSO', (req, res) => {
   
-    fetch('http://192.168.1.25:8080/api/github/auth')
-    .then(response => {
-      if (!response.ok) {
-        res.send('Error!');
+  fetch('http://192.168.1.29:8080/api/github/auth')
+  .then(response => {
+    if (!response.ok) {
+      res.send('Error!');
 
-      } else {
-        res.send('I win!');
+    } else {
+      res.send('I win!');
 
-      }
-    })
-    .catch(error => {
-      console.log("error");
+    }
+  })
+  .catch(error => {
+    console.log("error");
+  });
+
+});
+
+
+
+
+app.post('/login', (req, res) => {
+
+
+  const { user, pass } = req.body;
+  
+
+  try {
+    connection.query("SELECT * FROM customers WHERE email = ? AND password = ?", [user, pass], function (err, recordset) {
+        if (err) {
+            console.log(err);
+            res.send(JSON.stringify("Error while querying database :- " + err));
+        } else {
+            if (recordset.length > 0) {
+                console.log("Usuario identificado correctamente");
+                res.status(200).send("Usuario identificado correctamente");
+            } else {
+                console.log("Usuario [" + user + "] no existe");
+                res.send("USUARIO NO EXISTE" + user + pass);
+            }
+        }
     });
+} catch (err) {
+    console.log("Error while querying database :- " + err);
+    res.send(JSON.stringify("Error while querying database :- " + err));
+}
 
 });
 
 
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.post('/login', passport.authenticate('local'), (req, res) => {
-  res.redirect('/');
-});
+  app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
 
 
-
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
